232fed290ab0f0466dcba6a72010771730645137
resolt <pdmmichaelsen@gmail.com>
2022-01-15 14:57:36 +0100

    Added basic server struct setup and very basic get endpoint as well as db connection with no migration or models as of yet

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("A lot of stuff will go here (heh)")
+	s := server{db: getDBConn()}
+	s.setupRoutes()
+	if err := s.r.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getDBConn() (db *gorm.DB) {
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	fmt.Println(dbPort)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Copenhagen", dbHost, dbUser, dbPass, dbName, dbPort)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+type server struct {
+	db *gorm.DB
+	r  *gin.Engine
+}
+
+func (s *server) setupRoutes() {
+	s.r = gin.Default()
+	s.r.GET("/basicget", s.getBasicGet())
+}
+
+func (s *server) getBasicGet() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, map[string]string{"test": "test"})
+	}
 }
