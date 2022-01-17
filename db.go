289a0aef6ea9815d44bde1cbcc6cbcafec72f111
resolt <pdmmichaelsen@gmail.com>
2022-01-17 21:34:19 +0100

    Changed models to allow full deletion (removed DeletedAt field)

--- db.go
+++ db.go
@@ -3,25 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func getDBConn() (db *gorm.DB) {
+//Get Gorm DB connection
+func getDB() (db *gorm.DB, err error) {
 	dbHost := os.Getenv("DB_HOST")
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
 	dbName := os.Getenv("DB_NAME")
 	dbPort := os.Getenv("DB_PORT")
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Copenhagen", dbHost, dbUser, dbPass, dbName, dbPort)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		logFatal(err)
-	}
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	return
 }
 
+//migrate database models
 func migrate(db *gorm.DB) (err error) {
 	if err = db.AutoMigrate(&User{}); err != nil {
 		return
@@ -34,10 +34,13 @@ func migrate(db *gorm.DB) (err error) {
 
 //User model
 type User struct {
-	gorm.Model
-	Name string `gorm:"unique"`
+	ID        uint   `gorm:"primarykey"`
+	Name      string `gorm:"unique"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
+//Gorm hook for deleting posts of user before deleting user
 func (u *User) BeforeDelete(tx *gorm.DB) (err error) {
 	posts := []Post{}
 	result := tx.Where("user_id = ?", u.ID).Find(&posts)
@@ -53,9 +56,11 @@ func (u *User) BeforeDelete(tx *gorm.DB) (err error) {
 
 //Post model
 type Post struct {
-	gorm.Model
-	User   User
-	UserID uint   `gorm:"uniqueIndex:post_idx"`
-	Title  string `gorm:"uniqueIndex:post_idx"`
-	Text   string
+	ID        uint   `gorm:"primarykey"`
+	User      User   ``
+	UserID    uint   `gorm:"uniqueIndex:post_idx"`
+	Title     string `gorm:"uniqueIndex:post_idx"`
+	Text      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
